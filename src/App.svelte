c2abffeb40b15236fad23d8ed091d2cf3958bdd8
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2022-09-02 11:30:25 +0200

    initial svelte app

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Header from "./components/Header.svelte";
+  import Footer from "./components/Footer.svelte";
+</script>
+
+<Header />
+<main />
+<Footer />
+
+<style>
+  main {
+    min-height: calc(100vh - 5rem - 7rem);
+  }
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,31 @@
+<script>
+</script>
+
+<header>
+  <h1><a href="/">robbe vanslambrouck</a></h1>
+  <nav />
+</header>
+
+<style>
+  header {
+    height: 7rem;
+    box-shadow: 3px 3px 10px 3px #ddd;
+  }
+  h1 {
+    display: inline-block;
+    font-size: 2.5rem;
+    font-weight: 600;
+    line-height: 7rem;
+    margin-left: 3rem;
+  }
+  a {
+    text-decoration: none;
+    text-transform: capitalize;
+    color: black;
+  }
+
+  a:hover {
+    color: #ff3e00;
+    text-decoration: underline;
+  }
+</style>
