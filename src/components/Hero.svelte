4c627290a7e6c6e7c0bbb91f9f29a91ed2207e0c
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2022-09-23 23:49:40 +0200

    removed typewritter effect

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,7 +1,5 @@
 <script>
   import { inview, addInview, removeInview } from "../stores";
-  import { onMount, onDestroy } from "svelte";
-  import Typewriter from "svelte-typewriter";
   import viewport from "../useViewportAction";
 
   const handleEnterViewport = () => {
@@ -20,21 +18,10 @@
   on:exitViewport={handleExitViewport}
 >
   <h2 class="sr-only">hero</h2>
-  <Typewriter interval={80} wordInterval={2000} mode="loopRandom" cursor={true}>
-    <p>frontend developer</p>
-    <p>coder</p>
-    <p>I love svelte</p>
-  </Typewriter>
 </section>
 
 <style>
   section {
     min-height: calc(100vh - 7rem);
-  }
-
-  p {
-    display: flex;
-    font-size: 5rem;
-    padding-top: 5rem;
   }
 </style>
